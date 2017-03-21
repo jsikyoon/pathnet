f8ef0db1f25084bb70da93a5ca83906c13271f5d
Yoon Jaesik (i338425) <jaesik.yoon01@sap.com>
2017-03-21 16:02:21 +0900

    .

--- binary_mnist_pathnet.py
+++ binary_mnist_pathnet.py
@@ -20,18 +20,25 @@ def train():
   mnist = input_data.read_data_sets(FLAGS.data_dir,
                                     one_hot=True,
                                     fake_data=FLAGS.fake_data)
-
+  total_tr_data, total_tr_label = mnist.train.next_batch(mnist.train._num_examples);
+  tr_data_5_6=total_tr_data[(total_tr_label[:,5]==1.0)|(total_tr_label[:,6]==1.0)];
+  tr_label_5_6=total_tr_label[(total_tr_label[:,5]==1.0)|(total_tr_label[:,6]==1.0)];
+  tr_label_5_6=tr_label_5_6[:,5:7]; tr_5_6_flag=0;
+  total_ts_data, total_ts_label = mnist.test.next_batch(mnist.test._num_examples);
+  ts_data_5_6=total_ts_data[(total_ts_label[:,5]==1.0)|(total_ts_label[:,6]==1.0)];
+  ts_label_5_6=total_ts_label[(total_ts_label[:,5]==1.0)|(total_ts_label[:,6]==1.0)];
+  ts_label_5_6=ts_label_5_6[:,5:7];
   sess = tf.InteractiveSession()
   # Create a multilayer model.
 
   # Input placeholders
   with tf.name_scope('input'):
     x = tf.placeholder(tf.float32, [None, 784], name='x-input')
-    y_ = tf.placeholder(tf.float32, [None, 10], name='y-input')
+    y_ = tf.placeholder(tf.float32, [None, 2], name='y-input')
 
   with tf.name_scope('input_reshape'):
     image_shaped_input = tf.reshape(x, [-1, 28, 28, 1])
-    tf.summary.image('input', image_shaped_input, 10)
+    tf.summary.image('input', image_shaped_input, 2)
 
   # geopath_examples
   geopath=pathnet.geopath_initializer(FLAGS.L,FLAGS.M);
@@ -75,8 +82,8 @@ def train():
   """
   
   # Do not apply softmax activation yet, see below.
-  output_weights=pathnet.module_weight_variable([FLAGS.filt,10]);
-  output_biases=pathnet.module_bias_variable([10]);
+  output_weights=pathnet.module_weight_variable([FLAGS.filt,2]);
+  output_biases=pathnet.module_bias_variable([2]);
   y = pathnet.nn_layer(net,output_weights,output_biases,'output_layer', act=tf.identity);
 
   with tf.name_scope('cross_entropy'):
@@ -112,10 +119,10 @@ def train():
   def feed_dict(train):
     """Make a TensorFlow feed_dict: maps data onto Tensor placeholders."""
     if train or FLAGS.fake_data:
-      xs, ys = mnist.train.next_batch(100, fake_data=FLAGS.fake_data)
+      xs=tr_data_5_6[tr_5_6_flag:tr_5_6_flag+16,:]; ys=tr_label_5_6[tr_5_6_flag:tr_5_6_flag+16,:];
       k = FLAGS.dropout
     else:
-      xs, ys = mnist.test.images, mnist.test.labels
+      xs=ts_data_5_6;ys=ts_label_5_6;
       k = 1.0
     return {x: xs, y_: ys}
     #return {x: xs, y_: ys, keep_prob: k}
@@ -131,26 +138,31 @@ def train():
     
     # First Candidate
     pathnet.geopath_insert(geopath,geopath_set[first],FLAGS.L,FLAGS.M);
-    for j in range(FLAGS.T):
-      summary_geo1_tr, _ = sess.run([merged, train_step], feed_dict=feed_dict(True))
+    acc_geo1_tr=0;
+    for j in range(FLAGS.T-1):
+      summary_geo1_tr, _, acc_geo1_tmp = sess.run([merged, train_step,accuracy], feed_dict=feed_dict(True))
+      acc_geo1_tr+=acc_geo1_tmp;
     run_options_geo1 = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
     run_metadata_geo1 = tf.RunMetadata()
-    summary_geo1_tr, _ = sess.run([merged, train_step],
+    summary_geo1_tr, _, acc_geo1_tmp = sess.run([merged, train_step,accuracy],
                               feed_dict=feed_dict(True),
                               options=run_options_geo1,
                               run_metadata=run_metadata_geo1)
+    acc_geo1_tr+=acc_geo1_tmp;
     summary_geo1_ts, acc_geo1 = sess.run([merged, accuracy], feed_dict=feed_dict(False))
-    
     # Second Candidate
     pathnet.geopath_insert(geopath,geopath_set[second],FLAGS.L,FLAGS.M);
-    for j in range(FLAGS.T):
-      summary_geo2_tr, _ = sess.run([merged, train_step], feed_dict=feed_dict(True))
+    acc_geo2_tr=0;
+    for j in range(FLAGS.T-1):
+      summary_geo2_tr, _, acc_geo2_tmp = sess.run([merged, train_step,accuracy], feed_dict=feed_dict(True))
+      acc_geo2_tr+=acc_geo2_tmp;
     run_options_geo2 = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
     run_metadata_geo2 = tf.RunMetadata()
-    summary_geo2_tr, _ = sess.run([merged, train_step],
+    summary_geo2_tr, _, acc_geo2_tmp = sess.run([merged, train_step,accuracy],
                               feed_dict=feed_dict(True),
                               options=run_options_geo2,
                               run_metadata=run_metadata_geo2)
+    acc_geo2_tr+=acc_geo2_tmp;
     summary_geo2_ts, acc_geo2 = sess.run([merged, accuracy], feed_dict=feed_dict(False))
     
     # Compatition between two cases
@@ -161,6 +173,12 @@ def train():
       train_writer.add_run_metadata(run_metadata_geo1, 'step%03d' % i);
       test_writer.add_summary(summary_geo1_ts, i);
       print('Accuracy at step %s: %s' % (i, acc_geo1));
+      print('Training Accuracy at step %s: %s' % (i, acc_geo1_tr/FLAGS.T));
+      if(acc_geo1_tr/FLAGS.T == 1.0):
+        print('Learning Done!!');
+        print('Optimal Path is as followed.');
+        print(geopath_set[first]);
+        exit(1);
     else:
       geopath_set[first]=np.copy(geopath_set[second]);
       pathnet.mutation(geopath_set[first],FLAGS.L,FLAGS.M,FLAGS.N);
@@ -168,6 +186,12 @@ def train():
       train_writer.add_run_metadata(run_metadata_geo2, 'step%03d' % i);
       test_writer.add_summary(summary_geo2_ts, i);
       print('Accuracy at step %s: %s' % (i, acc_geo2));
+      print('Training Accuracy at step %s: %s' % (i, acc_geo2_tr/FLAGS.T));
+      if(acc_geo2_tr/FLAGS.T == 1.0):
+        print('Learning Done!!');
+        print('Optimal Path is as followed.');
+        print(geopath_set[second]);
+        exit(1);
 
   """
   # Need to re-initialize variables
@@ -197,7 +221,7 @@ if __name__ == '__main__':
                       help='If true, uses fake data for unit testing.')
   parser.add_argument('--learning_rate', type=float, default=0.001,
                       help='Initial learning rate')
-  parser.add_argument('--max_steps', type=int, default=70,
+  parser.add_argument('--max_steps', type=int, default=100,
                       help='Number of steps to run trainer.')
   parser.add_argument('--dropout', type=float, default=0.9,
                       help='Keep probability for training dropout.')
@@ -205,17 +229,17 @@ if __name__ == '__main__':
                       help='Directory for storing input data')
   parser.add_argument('--log_dir', type=str, default='/tmp/tensorflow/mnist/logs/pathnet',
                       help='Summaries log directory')
-  parser.add_argument('--M', type=int, default=2,
+  parser.add_argument('--M', type=int, default=10,
                       help='The Number of Modules per Layer')
-  parser.add_argument('--L', type=int, default=2,
+  parser.add_argument('--L', type=int, default=3,
                       help='The Number of Layers')
-  parser.add_argument('--N', type=int, default=1,
+  parser.add_argument('--N', type=int, default=3,
                       help='The Number of Selected Modules per Layer')
-  parser.add_argument('--T', type=int, default=100,
+  parser.add_argument('--T', type=int, default=50,
                       help='The Number of epoch per each geopath')
-  parser.add_argument('--filt', type=int, default=500,
+  parser.add_argument('--filt', type=int, default=20,
                       help='The Number of Filters per Module')
-  parser.add_argument('--candi', type=int, default=10,
+  parser.add_argument('--candi', type=int, default=64,
                       help='The Number of Candidates of geopath')
   FLAGS, unparsed = parser.parse_known_args()
   tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
